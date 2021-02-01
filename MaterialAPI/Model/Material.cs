1745b303d94dac9cf9f5bc10ef3c9b264f054ee6
Harun Suljagic <harun.suljagic@hotmail.com>
2021-02-01 21:16:01 +0300

    Add project files.

---
+++ MaterialAPI/Model/Material.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaterialAPI.Model
+{
+    public class Material
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Quality { get; set; }
+        public long Price { get; set; }
+    }
+}
---
+++ MaterialAPI/Model/MaterialContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaterialAPI.Model
+{
+    public class MaterialContext : DbContext
+    {
+        public MaterialContext(DbContextOptions<MaterialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Material> Materials { get; set; }
+    }
+}
